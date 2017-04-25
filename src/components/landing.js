2d78433cd742dcdc90252efcf433c4ab482e0651
Lupe <lupe_m13_92@hotmail.com>
2017-04-25 20:20:55 -0300

    added hero patterns y favicons

--- src/components/landing.js
+++ src/components/landing.js
@@ -8,9 +8,9 @@ class Landing extends Component {
     return (
     <div>
     	<div className="hero">
-			<div className="logo">
-				<img src="/images/logo.svg" alt="logo"/>
-			</div>
+    		<div className="logo">
+    			<img src="/images/logo.svg" alt="logo"/>
+    		</div>
     		<div className="title">
     			<h1>Octolive</h1>
     		</div>	
@@ -19,44 +19,34 @@ class Landing extends Component {
     		</div>
     		<button className="btn btn-default"><i className="fa fa-github" aria-hidden="true"></i>Sign in</button>
     	</div>
-    	<div className="description marketing">
-    		<div className="container block">
-    			<div className=" center col-lg-4 col-sm-4 ">
-    				<img className="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140"/>
-    				<h2>Heading</h2>
-    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    				<button className="button btn btn-default" href="#" role="button"><a>View details &raquo;</a></button>
-    			</div>
-    			<div className="center col-lg-4 col-sm-4 ">
-    				<img className="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140"/>
-    				<h2>Heading</h2>
-    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    				<button className="button btn btn-default" href="#" role="button"><a>View details &raquo;</a></button>
-    			</div>
-    			<div className="center col-lg-4 col-sm-4 ">
-    				<img className="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140"/>
-    				<h2>Heading</h2>
-    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    				<button className="button btn btn-default" href="#" role="button"><a>View details &raquo;</a></button>
+    	<div className="description">
+    		
+    		<div className="container marketing">
+    			<div className="row">
+    				<div className="block col-lg-4 col-sm-4">
+    					
+    					<p><i className="fa fa-snowflake-o fa-5x" aria-hidden="true"></i></p>
+    					<h2>Heading</h2>
+    					<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+    					<p><a className="btn btn-default" href="#" role="button">View details &raquo; 	</a></p>
+    				</div>
+    				<div className="block col-lg-4 col-sm-4">
+    					<p><i className="fa fa-user-circle" aria-hidden="true"></i></p>
+    					<h2>Heading</h2>
+    					<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+    					<p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    				</div>
+    				<div className="block col-lg-4 col-sm-4">
+    					<p><i className="fa fa-handshake-o" aria-hidden="true"></i></p>
+    					<h2>Heading</h2>
+    					<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+    					<p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    				</div>
     			</div>
     		</div>
     	</div>
     </div>
+
     )
   }
 }
