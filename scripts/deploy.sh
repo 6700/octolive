4b57ed08806c22669a2371d3aaef951dfd24d473
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-28 09:58:58 -0300

    try to make travis works

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,11 @@
+declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/XXXXX)"
+mv github_deploy_key $SSH_FILE
+chmod 600 "$SSH_FILE" \
+         && printf "%s\n" \
+              "Host github.com" \
+              "  IdentityFile $SSH_FILE" \
+              "  LogLevel ERROR" >> ~/.ssh/config
+git config --global user.email "build-travis@6700.com.ar"
+git config --global user.name "Travis CI"
 yarn install
 yarn run deploy
