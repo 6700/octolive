f21aee28506b088752ccead4b75c0d09cb3a8c95
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-28 10:24:22 -0300

    fixes for travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
-mkdir ~/ssh
-openssl aes-256-cbc -K $encrypted_e8ec75924147_key -iv $encrypted_e8ec75924147_iv -in github_deploy_key.enc -out ~/ssh/github_deploy_key -d
-GIT_SSH_COMMAND="ssh -i ~/ssh/github_deploy_key"
+mkdir -p ~/.ssh
+ls ~/.ssh
+openssl aes-256-cbc -K $encrypted_e8ec75924147_key -iv $encrypted_e8ec75924147_iv -in github_deploy_key.enc -out ~/.ssh/github_deploy_key -d
+GIT_SSH_COMMAND="ssh -i ~/,ssh/github_deploy_key"
 chmod 600 ~/ssh/github_deploy_key
 git config --global user.email "build-travis@6700.com.ar"
 git config --global user.name "Travis CI"
