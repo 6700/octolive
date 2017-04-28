601f26160d21c0b0de8f534da388bc009dbbcfd0
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-28 10:38:27 -0300

    fixes for travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
+ssh-add -l
 mkdir -p ~/.ssh
 ls ~/.ssh
 openssl aes-256-cbc -K $encrypted_e8ec75924147_key -iv $encrypted_e8ec75924147_iv -in github_deploy_key.enc -out ~/.ssh/id_rsa -d
@@ -8,5 +9,6 @@ chmod 600 ~/.ssh/id_rsa.pub
 git config --global user.email "build-travis@6700.com.ar"
 git config --global user.name "Travis CI"
 
-yarn install
+
+yarn install -s
 yarn run deploy
