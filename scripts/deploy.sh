5162a8175f27dcff3b5886e12c10043d08cbe892
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-28 10:46:25 -0300

    fixes for travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 eval `ssh-agent -s`
 ssh-add -L
-ssh-add ~/.ssh/id_rsa
-ssh-add -L
 mkdir -p ~/.ssh
 ls ~/.ssh
 openssl aes-256-cbc -K $encrypted_e8ec75924147_key -iv $encrypted_e8ec75924147_iv -in github_deploy_key.enc -out ~/.ssh/id_rsa -d
+ssh-add ~/.ssh/id_rsa
+ssh-add -L
 GIT_SSH_COMMAND="ssh -i ~/,ssh/id_rsa"
 mv github_deploy_key.pub ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
