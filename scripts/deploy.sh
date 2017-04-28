648c4be8e7a0f51061ae7c85610de854959eb895
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-28 10:05:45 -0300

    fixes for travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,5 @@
+openssl aes-256-cbc -K $encrypted_e8ec75924147_key -iv $encrypted_e8ec75924147_iv
+  -in github_deploy_key.enc -out github_deploy_key -d
 ls
 GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa_example"
 chmod 600 "$SSH_FILE" 
