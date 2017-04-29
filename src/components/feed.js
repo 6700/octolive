1157e83e853c0b847d0fcb70dbd26f42379c39b6
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2017-04-29 16:56:25 -0300

    edited feednotifications,added scrollbar on feed

--- src/components/feed.js
+++ src/components/feed.js
@@ -6,24 +6,39 @@ class Feed extends Component {
 
     return (
     <div className="col-xs-12 box"> 
-    	<div className="feed">
-    		<div className="col-xs-4">
-    			<div className="side-inputs">
-    				<input type="checkbox"/>
-    				<select className="selector-feed" name="select">
-    					<option value="value1" defaultValue>Marcar como leido </option> 
-    					<option value="value2">Archivar</option>
-    					<option value="value3">Recordarme más tarde</option>
-    				</select>
-    			</div>
-    		</div>
-    		<div className="refresh-bar ">
-    			<div className="col-xs-2 col-sm-1 col-lg-1 col-xs-offset-5 col-sm-offset-7 col-lg-offset-77">
-    				<button className="refresh-button"> <i className="fa fa-refresh" aria-hidden="true"></i></button>
-    			</div>
-    		</div>
-    		<FeedNotification />
-    	</div>
+      <div className="feed">
+          <div className="col-xs-4">
+             <div className="side-inputs">
+                <input type="checkbox"/>
+                <select className="selector-feed" name="select">
+                   <option value="value1" defaultValue>Marcar como leido </option> 
+                   <option value="value2">Archivar</option>
+                   <option value="value3">Recordarme más tarde</option>
+               </select>
+           </div>
+       </div>
+       <div className="refresh-bar ">
+            <div className="col-xs-2 col-sm-1 col-lg-1 col-xs-offset-5 col-sm-offset-7 col-lg-offset-77">
+                <button className="refresh-button"> <i className="fa fa-refresh" aria-hidden="true"></i></button>
+            </div>
+        </div>
+         <div className="scrollbar col-xs-12" id="style-default">
+            <div className="force-overflow">
+                <FeedNotification />
+                <FeedNotification />
+                <FeedNotification />
+                <FeedNotification />
+                <FeedNotification />
+                <FeedNotification />
+                <FeedNotification />
+                <FeedNotification />
+                <FeedNotification />
+                <FeedNotification />
+                <FeedNotification />
+                <FeedNotification />
+            </div>
+        </div>
+       </div>
     </div>
     )
   }
--- src/components/feednotification.js
+++ src/components/feednotification.js
@@ -5,7 +5,22 @@ class FeedNotification extends Component {
   render () {
 
     return (
-        <div>soy una notificación</div>
+	    <div className="feed-notification col-xs-12 ">
+	    	<div className="col-xs-2 col-sm-2 ">
+		    	<input type="checkbox" />
+			    <i className="bookmark fa-2x fa fa-bookmark " aria-hidden="true"></i>
+	
+	    	</div>
+
+	    	<div className="col-xs-7 col-sm-7 comment">
+	    		<p>Someone commented on your Pull request #19 </p> 
+	    	</div>
+	    	<div className="col-xs-3 col-sm-3 repo-name">
+	    		<p className="col-xs-10">6700/octolive</p>
+	    	</div>
+	    	<div className=" circle">
+	    	</div>
+	    </div>
     )
   }
 }
