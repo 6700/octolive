2825bc03633976e59faedf578614f217054067e6
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-29 11:14:20 -0300

    fix for uri issue

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   VALID_ORIGINS = ["octolive.xyz", "www.octolive.xyz", "localhost"].freeze
   def is_an_valid_origin? origin
-    Rails.logger.info origin
+    Rails.logger.info "El origen es: #{origin}"
     VALID_ORIGINS.include? URI.parse(origin).host
   end
 end
