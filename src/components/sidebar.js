efb754aa15e6d44e39ee2a2569a837482695b893
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2017-04-29 18:19:12 -0300

    editing side bar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -5,7 +5,25 @@ class Sidebar extends Component {
 
     return (
     	<div className="col-xs-12 sidebar"> 
-		
+		<div className="scrollbar2 col-xs-12" >
+            <div className="force-overflow">
+             	<div className="col-xs-12 linea">
+             		<div className="col-xs-2 icon">
+             			<i className="fa fa-envelope-open-o" aria-hidden="true"></i>
+             		</div>
+					<div className="col-xs-4">
+             			<p>Inbox</p>
+					</div>	
+					<div className="col-xs-offset-4 col-xs-2">
+						<div className="number">
+							<p>3</p>
+						</div>
+					</div>
+             	</div>
+             
+            	
+            </div>
+        </div>
     	</div>
     )
   }
