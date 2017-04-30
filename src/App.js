b336a95e97d9c690fabadbe128d7fa0ccd23b81f
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2017-04-29 23:38:11 -0300

    sidebar styling done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Feed from './components/feed.js'
 import FeedNotification from './components/feednotification.js'
 import Landing from './components/landing.js'
 import AuthenticationChest from './chests/authentication_chest';
+import SidebarLine from './components/sidebar-line'
+
 class App extends Component {
   render() {
     var component;
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
 import './sidebar.css';
+import SidebarLine from './sidebar-line';
 class Sidebar extends Component {
   render () {
 
     return (
-    	<div className="col-xs-12 sidebar"> 
-		<div className="scrollbar2 col-xs-12" >
-            <div className="force-overflow">
-             	<div className="col-xs-12 linea">
-             		<div className="col-xs-2 icon">
-             			<i className="fa fa-envelope-open-o" aria-hidden="true"></i>
-             		</div>
-					<div className="col-xs-4">
-             			<p>Inbox</p>
-					</div>	
-					<div className="col-xs-offset-4 col-xs-2">
-						<div className="number">
-							<p>3</p>
-						</div>
-					</div>
-             	</div>
-             
-            	
-            </div>
-        </div>
+    <div className="col-xs-12 sidebar"> 
+    	<div className="scrollbar2 col-xs-12" >
+    		<div className="force-overflow">
+
+    			<SidebarLine />
+    			<SidebarLine />
+    			<SidebarLine />
+    			<SidebarLine />
+    			<div className="col-xs-12">
+    				<hr className="style14" />
+    			</div>
+    			<SidebarLine />
+    			<SidebarLine />
+    			<SidebarLine />
+    			<SidebarLine />
+    			<div className="col-xs-12">
+    				<hr className="style14" />
+    			</div> 
+    			<SidebarLine />
+    			<SidebarLine />
+    			<SidebarLine />
+    			<SidebarLine />
+    		</div>
     	</div>
+    </div>
     )
   }
 }
