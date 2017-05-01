92e3b074edb3b012f8f767e1c80f0d28b8850b59
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2017-05-01 18:07:54 -0300

    dinamic sidebar

--- src/api_routes.js
+++ src/api_routes.js
@@ -3,6 +3,7 @@ export default (() => {
   return {
     sign_in_redirection: api_url + "/users/auth/github/redirect",
     access_token_validation: api_url + "/users/auth/github/callback",
-    profile: api_url + "/api/v1/users/me"
+    profile: api_url + "/api/v1/users/me",
+    notification: api_url + "/api/v1/notifications"
   }
 })()
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,23 +2,40 @@ import React, { Component } from 'react';
 import './sidebar.css';
 import SidebarLine from './sidebar-line';
 import Line from './line'
+import ApiRoutes from '../api_routes'
+const { f } = window;
 class Sidebar extends Component {
-  render () {
+    constructor (props) {
+        super(props)
+        this.state = {
+            notificationsCount: {}
+        }
+    }
 
+    componentDidMount() {
+        f(ApiRoutes.notification)
+            .then((content) => {
+                this.setState({
+                    notificationsCount: content
+                })
+            })
+    }
+
+  render () {
+                console.log(this.state);
     return (
     <div className="col-xs-12 sidebar"> 
-    	<div className="scrollbar2 col-xs-12" >
-    		<div className="force-overflow">
-
-    			<SidebarLine text="Inbox" icon="envelope-open-o" notificationsCount="3" />
-    			<SidebarLine text="Archived" icon="check" notificationsCount="2"/>
-                <SidebarLine text="Pending" icon="clock-o" notificationsCount="1"/>
-    			<SidebarLine text="Important" icon="exclamation" notificationsCount="1"/>
+        <div className="scrollbar2 col-xs-12" >
+            <div className="force-overflow">
+    			<SidebarLine text="Inbox" icon="envelope-open-o" notificationsCount={this.state.notificationsCount.inbox_count} />
+    			<SidebarLine text="Archived" icon="check" notificationsCount={this.state.notificationsCount.archived_count}/>
+                <SidebarLine text="Pending" icon="clock-o" notificationsCount={this.state.notificationsCount.pending_count}/>
+    			<SidebarLine text="Important" icon="exclamation" notificationsCount={this.state.notificationsCount.important_count}/>
                 <Line />
-    			<SidebarLine text="Releases" icon="tags" notificationsCount="1"/>
-    			<SidebarLine text="Pull Requests" icon="code-fork" notificationsCount="2"/>
-    			<SidebarLine text="Mentions" icon="commenting-o" notificationsCount="2"/>
-    			<SidebarLine text="Issues" icon="exclamation-circle" notificationsCount="2"/>
+    			<SidebarLine text="Releases" icon="tags" notificationsCount={this.state.notificationsCount.releases_count}/>
+    			<SidebarLine text="Pull Requests" icon="code-fork" notificationsCount={this.state.notificationsCount.pull_request_count}/>
+    			<SidebarLine text="Mentions" icon="commenting-o" notificationsCount={this.state.notificationsCount.mentions_count}/>
+    			<SidebarLine text="Issues" icon="exclamation-circle" notificationsCount={this.state.notificationsCount.issues_count}/>
 
     		</div>
     	</div>
