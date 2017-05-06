8f5b7bee08db2b918345f0f33c5b7b8049f36f0e
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-06 14:27:21 -0300

    Fixed enabled task for worker

--- app/workers/github_worker.rb
+++ app/workers/github_worker.rb
@@ -4,9 +4,9 @@ class GithubWorker
   def perform(*_args)
     User.find_each do |user|
       GithubInteractor.new(user).tap do |t|
-        #t.update_user_repositories
-        #t.update_pull_requests
-        t.update_issues
+        t.update_user_repositories
+        t.update_pull_requests
+        #t.update_issues
       end
     end
   end
