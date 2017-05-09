a5bcde5609f2f00c1fae142ddb129a53ceacca82
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-08 22:52:33 -0300

    Fixed broken relation

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,7 +2,7 @@ class Owner < ApplicationRecord
   Owner.inheritance_column = :owner_type
 
   has_many :repositories, dependent: :destroy
-  has_many :issues, dependent: :destroy
+  has_many :issues, through: :repositories, dependent: :destroy
 
   enum type: { user: 0, organization: 1 }
 end
