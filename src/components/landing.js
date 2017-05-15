6d03ae397941b835207a12c7fbec935d6617eead
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2017-05-15 17:01:57 -0300

    we are beta logo

--- src/components/landing.js
+++ src/components/landing.js
@@ -13,7 +13,7 @@ class Landing extends Component {
     <div>
     	<div className="hero">
     		<div className="logo">
-    			<img src="/images/logo-octolive.png" alt="logo"/>
+    			<img src="/images/logo-octolive-beta.png" alt="logo"/>
     		</div>
     		<div className="title">
     			<h1>Octolive</h1>
