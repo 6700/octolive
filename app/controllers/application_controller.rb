34c5a4773a8faed8b253cdaaeb4d7df3bebf3ce4
hdf1986 <hugo.farji@wolox.com.ar>
2017-05-17 18:46:59 -0300

    Fixed amount

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def paginated_resources(resources)
-    @paginated_resources ||= resources.page(page).per(50)
+    @paginated_resources ||= resources.page(page).per(30)
   end
 
   def page
