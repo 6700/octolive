705fe8883e1833f7f3e1e4e2569191a078eaf030
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-19 01:23:59 -0300

    removed pagination

--- app/controllers/api/v1/repositories_controller.rb
+++ app/controllers/api/v1/repositories_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RepositoriesController < ApplicationController
   def index
-    render json: paginated(current_user.repositories.includes(:owner))
+    render json: current_user.repositories.includes(:owner)
   end
 end
