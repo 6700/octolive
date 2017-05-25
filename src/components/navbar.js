30603cde97f917402873f19c701e89789730f0fe
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-25 16:33:53 -0300

    Added link for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -68,7 +68,7 @@ class Navbar extends Component {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand" href="/">Octolive</a>
+            <a className="navbar-brand" href="#/">Octolive</a>
           </div>
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
